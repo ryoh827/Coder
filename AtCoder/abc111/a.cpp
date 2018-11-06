6c5ccc404d3d37d03feef73560ffb93e8e0a2b33
peta727 <rn.gjt25@gmail.com>
2018-11-06 12:49:04 +0900

    slove A of abc111

---
+++ AtCoder/abc111/a.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+#include <cmath>
+int main() {
+    int n;
+    cin >> n;
+    string ans = "";
+    for(int i = 0; i < 3; i++) {
+        int m = n % 10;
+        if (m == 9) {
+            ans += '1';
+        } else {
+            ans += '9';
+        }
+        n = n / 10;
+    }
+    reverse(ans.begin(), ans.end());
+    cout << ans << endl;
+    return 0;
+}
