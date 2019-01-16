8b7d0651618ab8883cc5fc87136ad8fba4d52539
peta727 <rn.gjt25@gmail.com>
2019-01-16 16:36:54 +0900

    :sparkles: Add Scanner class to TS

---
+++ lib/ts/Scanner.ts
@@ -0,0 +1,59 @@
+class Scanner {
+  private buffer = '';
+  private stdinQueue: any = [];
+  private lineBuffer = '';
+
+  constructor() {
+    process.stdin.resume();
+    process.stdin.setEncoding('utf8');
+    process.stdin.on(
+      'data',
+      (chunk): void => {
+        this.buffer += chunk;
+        while (this.stdinQueue.length > 0) {
+          const i = this.buffer.indexOf('\n');
+          if (i < 0) {
+            break;
+          }
+          let line = this.buffer.substr(0, i);
+          if (line.endsWith('\r')) {
+            line = line.slice(0, -1);
+          }
+          this.buffer = this.buffer.substr(i + 1);
+          this.stdinQueue.shift()(line);
+        }
+      },
+    );
+  }
+  public getLine(): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const i = this.buffer.indexOf('\n');
+      if (i >= 0 && this.stdinQueue.length == 0) {
+        let line = this.buffer.substr(0, i);
+        if (line.endsWith('\r')) {
+          line = line.slice(0, -1);
+        }
+        this.buffer = this.buffer.substr(i + 1);
+        resolve(line);
+      } else {
+        this.stdinQueue.push(resolve);
+      }
+    });
+  }
+
+  public close(): void {
+    process.stdin.pause();
+  }
+}
+
+// Usage example
+async function main() {
+  const sc = new Scanner();
+  const a = parseInt(await sc.getLine(), 10);
+  const b = parseInt(await sc.getLine(), 10);
+  const c = parseInt(await sc.getLine(), 10);
+  console.log(a + b + c);
+  sc.close();
+}
+
+main();
